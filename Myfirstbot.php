30b7c6b2381fa8db1d552c131c5c3a5f187cf596
Gourav8152-ai <goravsitbatch1@gmail.com>
2020-09-30 10:42:18 -0700

    Added Extra command and json

--- Myfirstbot.php
+++ Myfirstbot.php
@@ -5,37 +5,68 @@ $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
- $e = print_r($update);
- 
-//$chatId = reply_to_message_id;
+$e = print_r($update);
+
+#$chatId = reply_to_message_id;
 $chatId = $update["message"]["chat"]["id"];
-//$chatId = "-490195907";
 $gId = $update["message"]["from"]["id"];
 $userId = $update["message"]["from"]["id"];
 $firstname = $update["message"]["from"]["first_name"];
 $username = $update["message"]["from"]["username"];
 $message = $update["message"]["text"];
 $message_id = $update["message"]["message_id"];
+$r_userId = $update["message"]["reply_to_message"]["from"]["id"]; 
+$r_firstname = $update["message"]["reply_to_message"]["from"]["first_name"]; 
+$r_username = $update["message"]["reply_to_message"]["from"]["username"];
+$r_msg_id = $update["message"]["reply_to_message"]["message_id"];
 
-$username = urlencode($username);
-
-$info = "<b>Chat Id : </b><code>$chatId</code>%0A<b>First Name : </b><code>$firstname</code><b>Username : </b><code>$username</code><b>User Id: </b><code>$userId</code>";
-
+if($r_userId ===null)
+{
+   $info2 ="<b>ID : </b>$userId\n<b>First Name: </b>$firstname\n<b>Username : </b>@$username";
+   $info = urlencode($info2);
+}
+else{
+   $info2 ="<b>ID : </b>$r_userId\n<b>First Name: </b>$r_firstname\n<b>Username : </b>@$r_username";
+   $info = urlencode($info2);;
+}
 
 $cmds11 = "<b>Hey, welcome to this Bot!%0ACommands List:<b>To know the info</b><code>$info</code>";
 
 switch($message) {
-       
-        case "/start":
-                sendMessage($chatId, "<b>Type /cmds to know the command! </b>");
-                break;
-        case "/cmds":
-                sendMessage($chatId,"This are the command'.$cmds11.'");
-                break;
-        case "/info":
-                sendMessage($chatId,$info);
-                break;
-        }
+
+      
+
+       case "/start":
+
+               sendMessage($chatId, "Hey Type  /cmds for a list of all commands!");
+
+               break;
+
+       case "/cmds":
+
+               sendMessage($chatId, $cmds11);
+
+               break;
+
+       case "/info":
+
+               sendMessage($chatId,$info);
+
+               break;
+
+       case "/status":
+
+             sendMessage($chatId, "<b>I AM NOT DEAD </b>");
+
+             break;
+
+        case "/id":
+
+               sendMessage($chatId, "<b>CHAT ID : </b><code>$chatId</code>");
+
+               break;
+
+}
 
 function sendMessage ($chatId, $message) {
        
