e993933a1b59e434a97a8353abeeb86c1ecc429b
Umesh Garg <67823774+Joker9050@users.noreply.github.com>
2024-10-05 14:15:07 +0530

    Update Myfirstbot.php
    I had fix the bug : 
    1. this bot is not responding so i made changes in the sendmessage function.
    2. upgraded the whole code to OOPS php
    3. Updated the /cmds reply portion .

--- Myfirstbot.php
+++ Myfirstbot.php
@@ -1,82 +1,91 @@
-<?php 
- #one things you need to webhook the file using a https host to run it
-$botToken = "token"; #replace the token from the token you get from bot father 
-$website = "https://api.telegram.org/bot".$botToken;
-error_reporting(0);
-$update = file_get_contents('php://input');
-$update = json_decode($update, TRUE);
-$e = print_r($update);
+<?php
 
-#$chatId = reply_to_message_id;
-$chatId = $update["message"]["chat"]["id"];
-$gId = $update["message"]["from"]["id"];
-$userId = $update["message"]["from"]["id"];
-$firstname = $update["message"]["from"]["first_name"];
-$username = $update["message"]["from"]["username"];
-$message = $update["message"]["text"];
-$message_id = $update["message"]["message_id"];
-$r_userId = $update["message"]["reply_to_message"]["from"]["id"]; 
-$r_firstname = $update["message"]["reply_to_message"]["from"]["first_name"]; 
-$r_username = $update["message"]["reply_to_message"]["from"]["username"];
-$r_msg_id = $update["message"]["reply_to_message"]["message_id"];
+class TelegramBot {
+    private $botToken;
+    private $website;
+    private $update;
 
-if($r_userId ===null)
-{
-   $info2 ="<b>ID : </b>$userId\n<b>First Name: </b>$firstname\n<b>Username : </b>@$username";   
-}
-else{
-   $info2 ="<b>ID : </b>$r_userId\n<b>First Name: </b>$r_firstname\n<b>Username : </b>@$r_username";
-}
-
-$info = urlencode($info2);
-
-$cmds11 = "<b>Hey, welcome to this Bot!%0ACommands List:<b>To know the info</b><code>$info</code>";
-
-$message_text = "";
-
-switch($message) {
-
-      
-
-       case "/start":
-               $message_text = "Hey Type  /cmds for a list of all commands!";
-               
-
-               break;
-
-       case "/cmds":
-
-               $message_text = $cmds11;
-
-               break;
-
-       case "/info":
-
-               $message_text = $info;
-
-               break;
-
-       case "/status":
-
-             $message_text = "<b>I AM NOT DEAD </b>";
-
-             break;
-
-        case "/id":
-
-               $message_text = "<b>CHAT ID : </b><code>$chatId</code>";
-
-               break;
-
-}
-
-sendMessage($chatId, $message_text);
-
-function sendMessage ($chatId, $message) {
-       
-        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".$message."&reply_to_message_id=".$message_id."&parse_mode=HTML";
+    public function __construct($token) {
+        $this->botToken = $token;
+        $this->website = "https://api.telegram.org/bot" . $this->botToken;
+        $this->update = json_decode(file_get_contents('php://input'), TRUE);
+    }
+    
+    /**
+     * Method to give the chat id
+     */
+    public function getChatId() {
+        return $this->update["message"]["chat"]["id"];
+    }
+    
+    /**
+     * Function to give the user information
+     * @return Array 
+     */
+    public function getUserInfo() {
+        $userId = $this->update["message"]["from"]["id"];
+        $firstname = $this->update["message"]["from"]["first_name"];
+        $username = isset($this->update["message"]["from"]["username"]) ? $this->update["message"]["from"]["username"] : '';
+        return [
+            "userId" => $userId,
+            "firstname" => $firstname,
+            "username" => $username
+        ];
+    }
+    
+    /**
+     * Method to get the input message from user
+     * @return String 
+     */
+    public function getMessage() {
+        return $this->update["message"]["text"];
+    }
+    
+    /**
+     * Method to send reply message 
+     */
+    public function sendMessage($chatId, $message) {
+        $url = $this->website . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&parse_mode=HTML";
         file_get_contents($url);
-       
+    }
+    
+    /**
+     * Method to handle the commands;
+     */
+    public function handleCommand() {
+        $message = $this->getMessage();
+        $chatId = $this->getChatId();
+        $userInfo = $this->getUserInfo();
+        
+        $info = "<b>ID : </b>{$userInfo['userId']}\n<b>First Name: </b>{$userInfo['firstname']}\n<b>Username : </b>@{$userInfo['username']}";
+
+        switch ($message) {
+            case "/start":
+                $response = "Hey Type /cmds for a list of all commands!";
+                break;
+            case "/cmds":
+                $response = "Hey, welcome to this Bot!\nCommands List:\n<b>To know the info- /info</b>\n<b>To know the id- /id</b>";
+                break;
+            case "/info":
+                $response = $info;
+                break;
+            case "/status":
+                $response = "<b>I AM NOT DEAD </b>";
+                break;
+            case "/id":
+                $response = "<b>CHAT ID : </b><code>$chatId</code>";
+                break;
+            default:
+                $response = "Invalid command.";
+        }
+
+        $this->sendMessage($chatId, $response);
+    }
 }
+
+// Initialize the bot and handle the command
+// paste the telegram bot in example:- TelegramBot("XXXXXXXXXXXXXXX"); 
+$bot = new TelegramBot("");
+$bot->handleCommand();
 
 ?>
